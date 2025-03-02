44e4bc8f11d5154a56fbb4f69eede64a8c5d646b
bodhiintellect <nath.syam.1986@gmail.com>
2025-03-02 10:39:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,21 @@ self.addEventListener("install", event => {
 		caches.open(cacheName).then(cache => {
 			return cache.addAll([
 				"index.html",
+				"main.js",
+				"style.css",
+				"manifest.json",
+				"favicon.ico",
+				"sw.js",
+				"android-chrome-36x36.png",
+				"android-chrome-48x48.png",
+				"android-chrome-72x72.png",
+				"android-chrome-96x96.png",
+				"android-chrome-144x144.png",
+				"android-chrome-192x192.png",
+				"android-chrome-256x256.png",
+				"android-chrome-384x384.png",
+				"android-chrome-512x512.png",
+				"maskable_icon.png",
 				// "android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
 				// "android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
 				// "android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
